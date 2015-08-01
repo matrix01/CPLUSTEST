1000d466207e06f332bd42e363ab2161bfe91adc
Md. Milan MIa <matrix@Mds-Mac.local>
2015-08-01 14:30:04 +0600

    Initial Commit

---
+++ CPLUSTEST/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CPLUSTEST
+//
+//  Created by Md. Milan MIa on 8/1/15.
+//  Copyright (c) 2015 Md. Milan MIa. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
